990b91e1ca2c127f7d9a51316660e5d496c292ef
mishkasv <misha.savcuk@gmail.com>
2021-06-19 22:41:43 +0300

    add homework15

--- cursor_git/homework15_git/static/js/blog/menu.js
+++ cursor_git/homework15_git/static/js/blog/menu.js
@@ -1,6 +1,5 @@
 $(document).ready(function () {
     $.ajax({
-        method: "GET",
         url: "/menu-items",
         success: function (data) {
             if (data.success) {
@@ -9,6 +8,7 @@ $(document).ready(function () {
                         "                    <a class=\"nav-link active\" aria-current=\"page\" href=\""+ el.link +"\">" + el.name + "</a>\n" +
                         "                </li>"
                     $("#menuItemsList").append(menuItemHtml);
+                    $("#menuItemsListfooter").append(menuItemHtml);
                 }
             }
         }
