e1275705b71338a19b9f85922dbbb6d88ab8d6e8
mishkasv <misha.savcuk@gmail.com>
2021-07-31 14:28:34 +0300

    add homework 25 and 26

---
+++ cursor_git/djangoProject/testapp/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+from .models import City,Food,Country,Restaurant,Personal,Menu
+
+admin.register(City)
+admin.register(Food)
+admin.register(Country)
+admin.register(Restaurant)
+admin.register(Personal)
+admin.register(Menu)
+# Register your models here.
