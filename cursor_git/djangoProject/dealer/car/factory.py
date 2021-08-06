6f7f38b09699cbcd464b54a0350a5e827e82ef2b
mishkasv <misha.savcuk@gmail.com>
2021-08-06 22:37:27 +0300

    add change homework 27

--- cursor_git/djangoProject/dealer/car/factory.py
+++ cursor_git/djangoProject/dealer/car/factory.py
@@ -1,9 +1,11 @@
 from . import models
 import factory, faker, random, datetime
 import factory.fuzzy
-import faker_vehicle
-
-FUELTYPE = ('gas', 'gasolin', 'dizel', 'electrical')
+from faker_vehicle.vehicle_dict import vehicles
+vehicle_model = [v['Model'] for v in vehicles]
+vehicle_category=[v['Category'] for v in vehicles]
+vehicle_make = [v['Make'] for v in vehicles]
+FUELTYPE = ('gas', 'gasolin', 'disel', 'electrical')
 ENGINETYPE = ('Straight Engine', 'Inline Engine', 'Flat Engine', 'V Engine')
 
 
@@ -13,14 +15,11 @@ class CarFactory(factory.django.DjangoModelFactory):
         #django_get_or_create = ('model',)
 
     color = factory.SubFactory('dealer.car.factory.ColorFactory')
-    model = factory.SubFactory(
-        'dealer.car.factory.ModelcarFactory',
-        name = random.choice(faker_vehicle.vehicles)['Model'],
-    )
+    model = factory.SubFactory('dealer.car.factory.ModelcarFactory')
     enginetype = factory.fuzzy.FuzzyChoice(ENGINETYPE)
     pollutant_class = factory.fuzzy.FuzzyChoice(models.Car.POLLUTE)
     price = factory.fuzzy.FuzzyInteger(3000,500001,100)
-    fueltype = factory.fuzzy.FuzzyChoice(FUELTYPE)
+    fueltype = factory.SubFactory('dealer.car.factory.FuelTypeFactory')
     status = factory.fuzzy.FuzzyChoice(models.Car.STATUS)
     doors = factory.fuzzy.FuzzyChoice([3,5])
     capacity = factory.fuzzy.FuzzyChoice([20, 25, 30, 35, 40, 45, 50])
@@ -31,7 +30,7 @@ class CarFactory(factory.django.DjangoModelFactory):
         'Semi-automatic and dual-clutch transmissions'
     ])
     number = factory.fuzzy.FuzzyChoice([5, 6, 7])
-    slug = factory.fuzzy.FuzzyText(random.choice(faker_vehicle.vehicles)['Make'] + '/' + random.choice(faker_vehicle.vehicles)['Model'])
+    slug = factory.fuzzy.FuzzyChoice(vehicle_model).fuzz()+'/'+factory.fuzzy.FuzzyChoice(vehicle_make).fuzz()
     sittingplace = factory.fuzzy.FuzzyChoice([2, 4, 5, 6, 7, 8, 9])
     firstragistrationdate = factory.fuzzy.FuzzyDate(datetime.datetime(1990, 1, 1), datetime.datetime(2021, 6, 6))
     enginepower = factory.fuzzy.FuzzyInteger(50,500)
@@ -50,7 +49,7 @@ class ModelcarFactory(factory.django.DjangoModelFactory):
         model = models.Model_car
         django_get_or_create = ('name',)
 
-    name = factory.fuzzy.FuzzyChoice(faker_vehicle.vehicles).fuzz()['Model']
+    name = factory.fuzzy.FuzzyChoice(vehicle_model)
     brand = factory.SubFactory('dealer.car.factory.BrandFactory')
 
 
@@ -59,7 +58,7 @@ class BrandFactory(factory.django.DjangoModelFactory):
         model = models.Brand
         django_get_or_create = ('name',)
 
-    name = factory.fuzzy.FuzzyChoice(faker_vehicle.vehicles).fuzz()['Make']
+    name = factory.fuzzy.FuzzyChoice(vehicle_make)
 
 
 class PictureFactory(factory.django.DjangoModelFactory):
@@ -71,3 +70,11 @@ class PictureFactory(factory.django.DjangoModelFactory):
     url = faker.Faker().file_path(depth=5, category='image', extension='img')
     position = 'position'
     metadata = 'metadata'
+
+class FuelTypeFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = models.FuelType
+        django_get_or_create = ('name',)
+
+
+    name = factory.fuzzy.FuzzyChoice(FUELTYPE)
