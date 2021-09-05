e19e44af83b27a9b08e6ce7edd6be942008ee542
mishkasv <misha.savcuk@gmail.com>
2021-09-05 21:16:16 +0300

    add last all homeworks (not all tests)

--- cursor_git/djangoProject/dealer/properties/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from dealer.orders import views
-
-urlpatterns = [
-
-]
--- cursor_git/djangoProject/dealer/properties/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from dealer.orders.models import Order
-from dealer.cars.models import Car
-
-
-def list_orders(request):
-    orders = Order.objects.filter(car)
-
-# Create your views here.
